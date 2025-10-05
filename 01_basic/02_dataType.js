f0e05574888a16925029445a00fb823a8dc29692
anishhkr <anish87875@gmail.com>
2025-10-05 21:48:38 +0000

    dataType done

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //Treat all js code as newer version
+
+// alert("Anish") //We are not useing it because we use node Js instead of the browser.
+
+let name = "Anish Kumar"
+let age = 58
+let isLogedIn = false
+
+// num => 2 to power 52
+// bigint
+//string => "", ''
+// boolean => true, false
+// null => standalone value (Absence of value)
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // Undefined
+console.log(typeof null); // Object
+
