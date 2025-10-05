435c7f61594440b2fb74ea46a986084ab33fa66c
anishhkr <anish87875@gmail.com>
2025-10-05 20:40:53 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 58642
+let accountEmail = "anish@ies.com"
+var accountPassword = "anish12345"
+accountCity = "Bihar"
+
+// accountId = 89 // NOt Allowed
+accountEmail = "anish@kr.com"
+accountPassword = "4556265"
+accountCity = "UP"
+let accountState;
+
+/*
+Prefer not to use var:
+Because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
