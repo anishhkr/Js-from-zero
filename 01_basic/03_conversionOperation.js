1d4ec7bed3e90e0f29ef5829eab191db55ee61a6
anishhkr <anish87875@gmail.com>
2025-10-06 13:09:57 +0000

    Conversion of data type

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -25,6 +25,35 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber); // output => 33
-console.log(typeof stringNumber); // output => string
+// console.log(stringNumber); // output => 33
+// console.log(typeof stringNumber); // output => string
+
+
+/******************************CONVERSION****************************/
+
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello "
+let str2 = "Anish!"
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
 
